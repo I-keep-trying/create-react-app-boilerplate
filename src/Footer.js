8f941509181331a5e5e3a231c33a541687d3891a
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2020-07-31 13:18:55 -0400

    style update

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="App-footer">
+      Developed by Andrea Crego 2020
+      <a
+        href="https://github.com/I-keep-trying"
+        target="_blank"
+        rel="noopener noreferrer"
+      >{' '}
+        <i style={{ color: 'white' }} class="fab fa-github"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Footer
